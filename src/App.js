0f41bf9f03b9373d79c596e811b9f0c2b399008c
Serge Modin <modinsv@yandex.ru>
2021-01-28 13:53:41 +0300

    Parsing data from json with axios.get

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import Person from "./components/Person";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
-function App() {
+const App = () => {
+  //Application's effect
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  });
+
+  // Application's state
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  // Event handlers
+  const addName = (event) => {
+    event.preventDefault();
+    // Check for duplicates
+    const checkForDuplicate = persons.find((person) => person.name === newName);
+    if (typeof checkForDuplicate !== "undefined") {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
+      return;
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterChange = (event) => setNewFilter(event.target.value);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h2>Phonebook</h2>
+      <Filter value={newFilter} onChange={handleFilterChange} />
+
+      <h3>add a new</h3>
+      <PersonForm
+        onSubmit={addName}
+        valueName={newName}
+        onChangeName={handleNameChange}
+        valueNumber={newNumber}
+        onChangeNumber={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+      <Persons filter={newFilter} persons={persons} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Person = ({ name, number }) => {
+  return (
+    <div>
+      {name} {number}
+      <br />
+    </div>
+  );
+};
+
+export default Person;
